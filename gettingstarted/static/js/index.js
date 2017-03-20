856184674e04786faa9c0f3c11115e663dba055e
donowhy <donowhy11@gmail.com>
2017-03-20 12:49:47 -0700

    sdf

--- gettingstarted/static/js/index.js
+++ gettingstarted/static/js/index.js
@@ -2,30 +2,30 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.carousel.carousel-slider').carousel();
-    $( "#frontSlider" ).on( "swipe", swipeHandler );
-
-    function swipeHandler(event){
-      console.log("swiped ", event);
-    }
-
-    var swiper = new Swiper('.swiper-container', {
-      spaceBetween: 30,
-      loop: true,
-      effect: 'flip',
-      centeredSlides: true,
-      // effect: 'coverflow',
-      // grabCursor: true,
-      // centeredSlides: true,
-      // slidesPerView: 'auto',
-      // coverflow: {
-      //   rotate: 50,
-      //   stretch: 0,
-      //   depth: 100,
-      //   modifier: 1,
-      //   slideShadows : true
-      // }
-    });
+    // $('.carousel.carousel-slider').carousel();
+    // $( "#frontSlider" ).on( "swipe", swipeHandler );
+    //
+    // function swipeHandler(event){
+    //   console.log("swiped ", event);
+    // }
+    //
+    // var swiper = new Swiper('.swiper-container', {
+    //   spaceBetween: 30,
+    //   loop: true,
+    //   effect: 'flip',
+    //   centeredSlides: true,
+    //   // effect: 'coverflow',
+    //   // grabCursor: true,
+    //   // centeredSlides: true,
+    //   // slidesPerView: 'auto',
+    //   // coverflow: {
+    //   //   rotate: 50,
+    //   //   stretch: 0,
+    //   //   depth: 100,
+    //   //   modifier: 1,
+    //   //   slideShadows : true
+    //   // }
+    // });
 
 
   }); // end of document ready
