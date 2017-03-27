61344a9d0a8b045c7d5751c0f843b26ab090e220
donowhy <donowhy11@gmail.com>
2017-03-26 17:13:12 -0700

    sdf

--- shareWear/models.py
+++ shareWear/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class test(models.Model):
+    user = models.ForeignKey(User)
+
 class profile(models.Model):
     user = models.ForeignKey(User)
     gender = models.BooleanField(default=True)
