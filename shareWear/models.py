93f8ce567afd41751f3a7f50946905e93f080833
donowhy <donowhy11@gmail.com>
2017-03-26 17:10:07 -0700

    sdf

--- shareWear/models.py
+++ shareWear/models.py
@@ -10,16 +10,16 @@ class profile(models.Model):
         return self.user.username
 
 class clothing(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
     carrier = models.CharField(max_length=100, null=True, blank=True)
-    carrier_id = models.CharField(max_length=100, null=True, blank=True)
+    carrier_id = models.CharField(max_length=200, null=True, blank=True)
     small_url = models.CharField(max_length=300, null=True, blank=True)
     large_url = models.CharField(max_length=300, null=True, blank=True)
     gender = models.BooleanField(default=True)
     price = models.CharField(max_length=100, null=True, blank=True)
     aff_url = models.CharField(max_length=300, null=True, blank=True)
     color = models.CharField(max_length=100, null=True, blank=True)
-    brand = models.CharField(max_length=100, null=True, blank=True)
+    brand = models.CharField(max_length=200, null=True, blank=True)
     cloth_type = models.CharField(max_length=100, null=True, blank=True)
 
     def __unicode__(self):
