82c5b7a9a3eaee33c21b84d5e91534ab063a6004
donowhy <donowhy11@gmail.com>
2017-04-13 21:42:48 -0700

    social auth

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shareWear',
-    'social_django',
+    'social.apps.django_app.default',
 )
 
 AUTHENTICATION_BACKENDS = (
