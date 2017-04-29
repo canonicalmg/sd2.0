0b34b97fa0e2d5c5ca77eace894a426f6b617ea6
donowhy <donowhy11@gmail.com>
2017-04-29 15:48:32 -0700

    filters

--- gettingstarted/static/js/discover.js
+++ gettingstarted/static/js/discover.js
@@ -3,6 +3,18 @@
         // $('select').material_select();
         $('.button-collapse').sideNav();
         $('.carousel').carousel();
+        $('input.autocomplete').autocomplete({
+            data: {
+                "Apple": null,
+                "Microsoft": null,
+                "Google": 'http://placehold.it/250x250'
+            },
+            limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+            onAutocomplete: function(val) {
+                // Callback function when value is autcompleted.
+            },
+            minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 $(document).ready(function() {
@@ -60,7 +72,8 @@ function populate_product(new_search){
                 data: {
                     'gender': GENDER,
                     'offset': offsetVar,
-                    'tags':TAG_LIST},
+                    'tags':TAG_LIST,
+                    'brands': $("#brandSelect").val()},
                 success: function (json) {
                     // console.log("json = ", json);
 
