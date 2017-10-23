396356f76c14737b66f39ba0eb7ab21c2b8f9b4a
donowhy <donowhy11@gmail.com>
2017-10-22 17:25:54 -0700

    offset category scrape

--- plr_app/urls.py
+++ plr_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.signUpLogIn, name='signUp'),
     url(r'^scrape/$', views.scrape_content, name='scrape_content'),
+    url(r'^scrape/(?P<url>[-\w]+)/(?P<category>\w+)/$', views.scrape_content_with_url, name='scrape_content'),
 ]
--- plr_app/views.py
+++ plr_app/views.py
@@ -14,3 +14,19 @@ def scrape_content(request):
     context = {'article': all_articles[0]}
 
     return HttpResponse(template.render(context, request))
+
+def scrape_content_with_url(request, url, category):
+    # hit url with /scrape/(what would be after category, separated by periods)/category
+
+    # http://plrplr.com/category/acne/page/13/
+    try:
+        url_string = "http://plrplr.com/category/"
+        url = url.split("-")
+        for each_item in url:
+            url_string += "%s/" % each_item
+        get_articles_from_category(url_string, category)
+    except:
+        print "error, items formatted incorrectly"
+        return HttpResponse("items may have been formatted improperly")
+
+    return HttpResponse("done with offset scrape")
